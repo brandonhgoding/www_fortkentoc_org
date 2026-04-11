61e024b96fb20edc70c824d12e8139b33dfbf1fd
Brandon Goding <brandon.h.goding@gmail.com>
2026-04-11 19:58:19 -0400

    feat(fkoc): add ContentPage template wrapper

---
+++ src/templates/ContentPage.jsx
@@ -0,0 +1,31 @@
+import PageHeader from '../components/layout/PageHeader';
+import './ContentPage.css';
+
+function ContentPage({ crumb, title, lede, toc, children }) {
+  return (
+    <div className="content-page">
+      <PageHeader crumb={crumb} title={title} lede={lede} />
+      <div className="content-page__body">
+        <div className="content-page__inner">
+          {toc && toc.length > 0 ? (
+            <div className="content-page__wrap">
+              <aside className="content-page__toc">
+                <h5>On this page</h5>
+                {toc.map((item) => (
+                  <a key={item.id} href={`#${item.id}`}>
+                    {item.label}
+                  </a>
+                ))}
+              </aside>
+              <main className="content-page__main">{children}</main>
+            </div>
+          ) : (
+            <main className="content-page__main">{children}</main>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContentPage;
